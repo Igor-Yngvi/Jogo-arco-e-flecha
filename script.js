d90f6d697411faba5cad24f5a77192e476671c32
Igor-Yngvi <96557071+Igor-Yngvi@users.noreply.github.com>
2023-04-15 11:32:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const li1 = document.querySelector("li:nth-child(1) input");
+const li2 = document.querySelector("li:nth-child(2) input");
+const li3 = document.querySelector("li:nth-child(3) input");
+const li4 = document.querySelector("li:nth-child(4) input");
+const li5 = document.querySelector("li:nth-child(5) input");
+const li6 = document.querySelector("li:nth-child(6) input");
+const li7 = document.querySelector("li:nth-child(7) input");
+const li8 = document.querySelector("li:nth-child(8) input");
+const li9 = document.querySelector("li:nth-child(9) input");
+
+li1.style.animationDuration = Math.floor(Math.random() * 5 + 0.3727142934)+"s";
+li2.style.animationDuration = Math.floor(Math.random() * 5 + 0.3727142934)+"s";
+li3.style.animationDuration = Math.floor(Math.random() * 5 + 0.3727142934)+"s";
+li4.style.animationDuration = Math.floor(Math.random() * 5 + 0.3727142934)+"s";
+li5.style.animationDuration = Math.floor(Math.random() * 5 + 0.3727142934)+"s";
+li6.style.animationDuration = Math.floor(Math.random() * 5 + 0.3727142934)+"s";
+li7.style.animationDuration = Math.floor(Math.random() * 5 + 0.3727142934)+"s";
+li8.style.animationDuration = Math.floor(Math.random() * 5 + 0.3727142934)+"s";
+li9.style.animationDuration = Math.floor(Math.random() * 5 + 0.3727142934)+"s";
